29eeb9bd811fe64d51f06be26a73e6aa5bea053c
Max Pham <max.pham@mail.utoronto.ca>
2020-03-08 04:15:51 -0400

    Hello World

--- src/main/java/com/example/linkshortener/LinkShortenerApplication.java
+++ src/main/java/com/example/linkshortener/LinkShortenerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkShortenerApplication {
 
+	// "./mvnw clean package" to build jar - "java -jar target/<whatever>.jar" to run.
+	// "./mvnw spring-boot:run" to run
+
 	public static void main(String[] args) {
 		SpringApplication.run(LinkShortenerApplication.class, args);
 	}
